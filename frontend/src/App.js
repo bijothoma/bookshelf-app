7c3dda8b2d67881682d9c337d92f6e31a127cb7c
Bijo Thomas <bijothoma@gmail.com>
2024-04-16 18:50:33 +0530

    bookshelf app

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="container">
+        <div className="current"></div>
+        <div className="social"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
