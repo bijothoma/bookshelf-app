09455014723a40b37bba4154a5cc6150f2425d65
Bijo Thomas <bijothoma@gmail.com>
2024-04-29 22:14:02 +0530

    useNavigate to reroute to dashboard

--- frontend/src/pages/logout.jsx
+++ frontend/src/pages/logout.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Logout = () => {
+  const navigate = useNavigate();
     useEffect(() => {
-        localStorage.setItem("userData","");
-        window.location.href = '/dashboard';
+        localStorage.removeItem("userData");
+        //window.location.href = '/dashboard';
+        navigate("/dashboard");
     },[])
   return (
     <div>logout</div>
